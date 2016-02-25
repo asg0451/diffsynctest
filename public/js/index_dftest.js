7de36b4f7733d531e5230e36dacf4c0a8d69ee2f
Miles Frankel <miles.frankel@gmail.com>
2016-02-25 00:07:24 -0500

    added support for multiple ids

--- public/js/index_dftest.js
+++ public/js/index_dftest.js
@@ -1,34 +1,42 @@
-var id = 'pooptest';
+var defaultId = 'pooptest';
 
 var DiffSyncClient = diffsync.Client || require('diffsync').Client
-
 var socket = window.io || require('socket.io-client')
 
-// pass the connection and the id of the data you want to synchronize
-var client = new DiffSyncClient(socket('http://' + window.location.hostname + ':4000'), id);
+var client;
+
+var startSync = function () {
+    if(!client) {
+        var id = document.getElementById('dsIdEntry').value;
+
+        client = new DiffSyncClient(socket('http://' + window.location.hostname + ':4000'), id);
+
+        client.on('connected', function(){
+            // the initial data has been loaded,
+            // you can initialize your application
+            if(!textarea) textarea = document.getElementById('dataArea');
+            data = client.getData();
+            console.log('connected');
+        });
+
+        client.on('synced', function(){
+            // an update from the server has been applied
+            // you can perform the updates in your application now
+            if(!textarea) textarea = document.getElementById('dataArea');
+            textarea.value = data.textValue;
+        });
+
+        client.initialize();
+    }
+}
+
 
 var data = {};
 
 var textarea;
 
-client.on('connected', function(){
-    // the initial data has been loaded,
-    // you can initialize your application
-    if(!textarea) textarea = document.getElementById('dataArea');
-    data = client.getData();
-    console.log('connected');
-});
-
-client.on('synced', function(){
-    // an update from the server has been applied
-    // you can perform the updates in your application now
-    if(!textarea) textarea = document.getElementById('dataArea');
-    textarea.value = data.textValue;
-});
-
-client.initialize();
-
 var syncTextArea = function() {
+    if(!client) return;
     if(!textarea) textarea = document.getElementById('dataArea');
     data.textValue = textarea.value;
     client.sync();
